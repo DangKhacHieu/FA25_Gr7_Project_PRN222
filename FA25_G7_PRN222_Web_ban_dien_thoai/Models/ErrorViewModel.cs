7b5a8445360b5faffd1c4879febad64c600a4aef
DangKhacHieu <khachieulx@gmail.com>
2025-10-05 15:00:13 +0700

    push project

---
+++ FA25_G7_PRN222_Web_ban_dien_thoai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FA25_G7_PRN222_Web_ban_dien_thoai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
