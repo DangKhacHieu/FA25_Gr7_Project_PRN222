2c2767368c15232fbc237942dfed9a8d9cc59f3c
Phong Hải <tranphonghaivl@gmail.com>
2025-10-31 22:02:59 +0700

    commit truoc khi pull main

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a24ad400f96d220615fd2791336058386b9e513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a4ed9d30afc904fe9b9424ca63c5e402c41fa8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
