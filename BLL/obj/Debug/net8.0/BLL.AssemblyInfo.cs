7f0e2ae02c944d13ca1471692bec3223e1bba23c
Phong Hải <tranphonghaivl@gmail.com>
2025-10-31 22:12:11 +0700

    mesh main

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2767368c15232fbc237942dfed9a8d9cc59f3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc7b08010bb5d9376c85f322ef63064f602a6ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
